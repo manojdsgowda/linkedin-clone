0777b71b7febc0e903c6b8fbc6cc032d12be93f0
Manoj Gowda <manojgowda@Manojs-MacBook-Air.local>
2023-01-31 22:15:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Routes>
+        <Route exact path='/' element={<Login />}></Route>
+      </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
